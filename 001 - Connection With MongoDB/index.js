885406c674f825f7f504e5589576dbba9d9e0cb0
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-09-06 10:57:58 +0530

    How to get Whole Data from Single Collection in MongoDB

--- 001 - Connection With MongoDB/index.js
+++ 001 - Connection With MongoDB/index.js
@@ -7,6 +7,7 @@ const PORT = 4000;
 
 const userModel = require("./connection");
 
+// post request to send data in db
 app.post("/register" , async (req,res) => {
 
     const {name} = req.body;
@@ -18,6 +19,16 @@ app.post("/register" , async (req,res) => {
     res.send(data);
 
 });
+
+// get request to get data from db
+app.get("/getuserdata", async (req,res) => {
+    
+    const userdata = await userModel.find();
+
+    return res.status(200).send(userdata);
+})
+
+
 
 app.listen(PORT , () => {
     console.log(`server start at port ${PORT}`);
